1e41a5bc29ba9c0bb4c6a94c5abf91a319014295
adrisad <22082002andrademedina@gmail.com>
2025-08-25 20:42:51 -0400

    feat(backend): instalando y implementando los cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import urlRoutes from "./modules/url/routes/url.routes.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando 🚀");
